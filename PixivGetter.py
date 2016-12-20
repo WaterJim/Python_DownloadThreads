881fc0dba187f2f2b4308554b493c3a93e3781cf
waterjim <waterjim90@gmail.com>
2016-12-20 21:26:07 +0800

    test part result
    测试部分爬取结果，发现没有拿到推荐列表=。=！

--- PixivGetter.py
+++ PixivGetter.py
@@ -19,7 +19,7 @@ import time
 '''
 ---------------------------------------------------------------
 '''
-baseUrl = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=60035216'  # 起始搜索页
+seedUrl = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=60035216'  # 起始搜索页
 baseSavePath = ''  # 保存路径
 baseTag = ()  # tag条件
 baseSize = {'width': 0, 'height': 0}  # 尺寸条件（width, height, 0则不限制）
@@ -27,6 +27,7 @@ baseViewCount = 2000  # 默认浏览数
 '''
 ---------------------------------------------------------------
 '''
+rootUrl = "http://www.pixiv.net"
 phantomPath = "E:\phantomjs-2.1.1-windows\\bin\phantomjs.exe"
 
 
@@ -40,23 +41,40 @@ def loadWebPageSource(targetUrl, webkitPath):
 
 
 def getAllNextLink(webHtml):
+    '''
+    获取当前页的推荐列表的所有连接
+    :param webHtml: 当前页的原文件
+    :return: 推荐栏的地址列表
+    '''
+    try:
+        soup = BeautifulSoup(webHtml, "html.parser")
+        nextLinkElement = soup.select('li.image-item > a')
+        nextLink = []
+        for element in nextLinkElement:
+            nextLink.append(rootUrl + element['href'])
+        return nextLink
+    except Exception:
+        print("error! get link from web html, exit")
+        return -1
     pass
 
 
 if __name__ == '__main__':
     print("启动Pixiv小爬虫......")
-    print("爬虫目标起始地址：" + baseUrl + str(1))
+    print("爬虫目标起始地址：" + seedUrl)
     time.sleep(3)
 
-    crawl_queue = [baseUrl]  # 保存待爬路径 （未去重）
+    crawl_queue = [seedUrl]  # 保存待爬路径 （未去重）
     seen_queue = set(crawl_queue)  # 保存待爬路径 （去重）
     while crawl_queue:
         url = crawl_queue.pop()
         html = loadWebPageSource(url, phantomPath)
         # 做点别的操作
-
+        print(html)
         # 获取下一个爬取路径
         for link in getAllNextLink(html):
             if link not in seen_queue:
+                print('new link : ' + link)
                 seen_queue.add(link)  # 保存新的路径
                 crawl_queue.append(link)  # 保存新的路径
+        time.sleep(5)  # 避免运行过快
